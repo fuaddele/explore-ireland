4c464fae039dda7df3a7c1a95896486a55d4ea33
FUAD OKIKIOLA DELE-GANIYU <fuad.deleganiyu.2022@mumail.ie>
2024-12-11 19:55:03 +0000

    first backend

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors"); // Import the cors package
+const path = require("path"); // Required for serving static files
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const mongoURI = "mongodb://localhost:27017/ireland";
+
+// Middleware to enable CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow your frontend origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
+    credentials: true, // Allow credentials to be included
+  })
+);
+
+// Connect to MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+// Serve static files from the "uploads" directory
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/reviews", require("./routes/review"));
+app.use("/api/attractions", require("./routes/attraction"));
+app.use("/api/profile", require("./routes/profile"));
+app.use("/api/help", require("./routes/helpfaq"));
+app.use("/api/faqs", require("./routes/faqRoutes"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
