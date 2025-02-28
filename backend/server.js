6aeaea44986be094ab2f5a89dfcbd1e62b27234f
FUAD OKIKIOLA DELE-GANIYU <fuad.deleganiyu.2022@mumail.ie>
2025-02-28 11:43:16 +0000

    removed local db

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/ireland";
+const mongoURI = process.env.MONGO_URI
 
 // Middleware to enable CORS
 app.use(
