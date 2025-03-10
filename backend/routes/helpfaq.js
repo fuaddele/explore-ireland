567a3b1fec503480ec2e6d21a0e4df75349b5325
FUAD OKIKIOLA DELE-GANIYU <fuad.deleganiyu.2022@mumail.ie>
2025-03-10 14:52:12 +0000

    removed all helpfaq code

--- backend/routes/helpfaq.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { addHelp } = require("../controllers/helpFaqController");
-const authMiddleware = require("../middleware/authMiddleware");
-const router = express.Router();
-
-router.post("/", authMiddleware, addHelp);
-
-module.exports = router;
